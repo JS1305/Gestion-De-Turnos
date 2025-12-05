cd950ba133b11a416fab6383faab3ced35a55eab
sgago23 <s.gago2003@gmail.com>
2025-12-05 11:22:33 +0100

    Corrección de errores

--- src/main/java/com/turnero/servlets/ListarTurnosServlet.java
+++ src/main/java/com/turnero/servlets/ListarTurnosServlet.java
@@ -8,11 +8,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 @WebServlet("/listaTurnos")
 public class ListarTurnosServlet extends HttpServlet {
 
+    private TurnoService turnoService = new TurnoService();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
